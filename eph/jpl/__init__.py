c2f7ce1b58da96ac8200af900b7842ff4318cfe9
bluephlavio <flavio.grandin@gmail.com>
2017-09-03 13:24:15 +0200

    jpl subpackage (__init__) documented.

--- eph/jpl/__init__.py
+++ eph/jpl/__init__.py
@@ -1,2 +1,21 @@
+""" :mod:`jpl` subpackage
+
+:mod:`jpl` subpackage contains modules related to retrieve and parse ephemeris from the `Jpl Horizons`_ ephemerides.
+
+The main classes defined by :mod:`subpackage` are
+
+* :class:`JplReq`: this class represents a Jpl request to the Horizons service.
+A :class:`JplReq` object is like a :class:`dict` containing Jpl service parameters used to specify an ephemeris.
+
+* :class:`JplRes`: this represents a Jpl response from the Horizons service.
+A :class:`JplRes` object contains the http response obtained from the Horizons service through a query
+performed by a :class:`JplReq` instance. The :class:`JplRes` object can parse the response in a
+`astropy`_ table.
+
+.. _`Jpl Horizons`: https://ssd.jpl.nasa.gov/?horizons
+.. _`astropy.table`: http://docs.astropy.org/en/stable/table/
+"""
+
+
 from .jpl import *
 
