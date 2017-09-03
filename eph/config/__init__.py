08039865401a720d93cbabebb2d64176086124f1
bluephlavio <flavio.grandin@gmail.com>
2017-09-03 13:15:43 +0200

    config docs created.

--- eph/config/__init__.py
+++ eph/config/__init__.py
@@ -1,1 +1,13 @@
+""" :mod:`config` subpackage
+
+:mod:`config` subpackage contains:
+
+* `config.ini`: configuration files containing all project wide default configuration.
+The file is copied at installation time to `~/.ephrc` so that it can be edited by the end user.
+If the installed file is deleted or not found by eph scripts `config.ini` configurations are kept.
+
+* :mod:`config` module: this python module defines functions related to create, edit and store configurations.
+
+"""
+
 from .config import *
