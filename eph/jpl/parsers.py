9d801b1b291603781cb189bf899b65c1f9f46dfd
bluephlavio <flavio.grandin@gmail.com>
2017-09-04 13:59:41 +0200

    jpl.parsers module documented.

--- eph/jpl/parsers.py
+++ eph/jpl/parsers.py
@@ -8,6 +8,18 @@ from .exceptions import JplParserError
 
 
 def get_sections(source):
+    """Split a Jpl Horizons ephemeris in header, data and footer.
+
+    Args:
+        source (str): the content of the Jpl Horizons ephemeris data output.
+
+    Returns:
+        :class:`tuple`: a tuple of strings containing header, data and footer sections respectively.
+
+    .. note:
+       Note that whitespaces and * are stripped out from section contents.
+
+    """
     m = re.match(r'(.*?)\$\$SOE(.*?)\$\$EOE(.*?)', source, flags=re.DOTALL)
     if m:
         to_strip = string.whitespace + '*'
@@ -17,11 +29,29 @@ def get_sections(source):
 
 
 def get_subsections(source):
+    """Split a source string in a list of sections separated by one or more *.
+
+    Args:
+        source (str): the source string to be splitted.
+
+    Returns:
+        :class:`list`: the lists of subsections.
+
+    """
     to_strip = string.whitespace
     return list(map(lambda ss: ss.strip(to_strip), re.split(r'\*{1,}', source)))
 
 
 def parse_data(data):
+    """Parses the data section of a Jpl Horizons ephemeris in a *list of lists* table.
+
+    Args:
+        data (str): the section containing data of a Jpl Horizons ephemeris.
+
+    Returns:
+        :class:`list`: the list of lists representing a data table.
+
+    """
     try:
         return numberify(parse_table(data))
     except:
@@ -29,12 +59,30 @@ def parse_data(data):
 
 
 def parse_cols(header):
+    """Finds and parses ephemeris column names in a Jpl Horizons ephemeris.
+
+    Args:
+        header (str): the header of a Jpl Horizons ephemeris.
+
+    Returns:
+        :class:`tuple`: a tuple with the names of columns.
+
+    """
     cols_subsection = get_subsections(header)[-1]
     cols = parse_row(cols_subsection)
     return tuple(cols)
 
 
 def parse(source):
+    """Parses an entire Jpl Horizons ephemeris and build an `astropy.table.Table`_ out of it.
+
+    Args:
+        source (str): the content of the Jpl Horizons data file.
+
+    Returns:
+        :class:`astropy.table.Table`: the table containing data from Jpl Horizons source ephemeris.
+
+    """
     header, ephemeris, footer = get_sections(source)
     data = transpose(parse_data(ephemeris))
     cols = parse_cols(header)
