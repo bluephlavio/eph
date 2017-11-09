f0b5734cd6e6fdb27a6934022ce1ff4e407b0921
bluephlavio <flavio.grandin@gmail.com>
2017-11-09 19:14:21 +0100

    Added shortcuts.py file

---
+++ eph/jpl/shortcuts.py
@@ -0,0 +1,11 @@
+"""Defines shortcut functions useful to ease the access of Jpl Horizons data.
+
+"""
+
+from .jpl import *
+
+def raw_data_from_req_file(req_file):
+    req = JplReq().read(req_file)
+    res = req.query()
+    return res.get_raw()
+
