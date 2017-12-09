de1277cb0552d11700f1cda953f218905deeb303
bluephlavio <flavio.grandin@gmail.com>
2017-12-09 20:26:10 +0100

    get, vec, pos and vel shortcut functions implemented.

--- eph/shortcuts.py
+++ eph/shortcuts.py
@@ -7,26 +7,31 @@ from .interface import JplReq
 
 def get(start, stop, obj, **kwargs):
     req = JplReq(
-        START_TIME=start,
-        STOP_TIME=stop,
-        COMMAND=obj,
-        **kwargs
-    )
-    req.obj_data = False
-    req.csv_format = True
+            START_TIME=start,
+            STOP_TIME=stop,
+            COMMAND=obj,
+            **kwargs,
+            OBJ_DATA=False,
+            CSV_FORMAT=True,
+            )
     res = req.query()
     return res.parse()
 
 
 def vec(start, stop, obj, **kwargs):
-    kwargs['VEC_LABELS'] = 'NO'
-    kwargs['TABLE_TYPE'] = 'VECTORS'
-    kwargs['VEC_TABLE'] = '3'
-    return get(start, stop, obj, **kwargs)
+    return get(start, stop, obj, **kwargs,
+               TABLE_TYPE='V',
+               VEC_LABELS=False
+               )
 
 
 def pos(start, stop, obj, **kwargs):
-    kwargs['VEC_LABELS'] = 'NO'
-    kwargs['TABLE_TYPE'] = 'VECTORS'
-    kwargs['VEC_TABLE'] = '1'
-    return get(start, stop, obj, **kwargs)
+    return vec(start, stop, obj, **kwargs,
+               VEC_TABLE=1
+               )
+
+
+def vel(start, stop, obj, **kwargs):
+    return vec(start, stop, obj, **kwargs,
+               VEC_TABLE=5
+               )
